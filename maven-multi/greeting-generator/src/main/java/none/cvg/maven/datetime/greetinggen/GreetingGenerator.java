3bfb3471a73e19d701ea37d57a982502986840df
Vinay.n <vinay.n@pravyuha.com>
2023-07-21 10:54:44 +0530

    Added adding numbers in the output

--- maven-multi/greeting-generator/src/main/java/none/cvg/maven/datetime/greetinggen/GreetingGenerator.java
+++ maven-multi/greeting-generator/src/main/java/none/cvg/maven/datetime/greetinggen/GreetingGenerator.java
@@ -2,10 +2,18 @@ package none.cvg.maven.datetime.greetinggen;
 
 import none.cvg.maven.greeting.Greeting;
 
+import java.util.Scanner;
+
 public class GreetingGenerator {
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(" Add operation  enter number 1");
+        int i=sc.nextInt();
+        System.out.println("enter number 2");
+        int j=sc.nextInt();
         Greeting greeting = new Greeting();
         System.out.println("\n" + greeting.greet());
+        System.out.println("\nadded number\n" + (i+j));
     }
 }
