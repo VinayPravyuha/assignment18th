0aafb427a0853fdcdae86d42a8a7e24677565ef9
VinayPravyuha <139948213+VinayPravyuha@users.noreply.github.com>
2023-07-22 14:54:37 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+
+Class Recursion{
+
+static int count=0;  
+static void p(){  
+count++;  
+if(count<=5){  
+System.out.println("hello "+count);  
+p();  
+} 
+}
+public static void main(String[] args){
+
+System.out.println("Enter the numner");
+int b=sc.nextInt();
+}}
+
